e2707ff1d38c2312243ba7f91a23802895435f7c
Egor Suldin <rd3tap@yandex.ru>
2024-08-21 00:49:55 +0300

    Check the creation of StringHandle instance
    Add unit test that checks the creation of StringHandle instance.

--- tests/unit/test_string_handle.py
+++ tests/unit/test_string_handle.py
@@ -11,6 +11,12 @@ class StringHandleTests(TestCase):
         pyitt.string_handle(s)
         string_handle_class_mock.assert_called_once_with(s)
 
+    @pyitt_native_patch('StringHandle')
+    def test_string_handle_creation(self, string_handle_class_mock):
+        s = 'my string'
+        pyitt.StringHandle(s)
+        string_handle_class_mock.assert_called_once_with(s)
+
 
 if __name__ == '__main__':
     unittest_main()  # pragma: no cover
