4de04099124547e89240b9c53898fe6b606f6a55
Egor Suldin <rd3tap@yandex.ru>
2024-08-21 00:49:31 +0300

    Check the creation of Id instance
    Add unit test that checks the creation of Id instance.

--- tests/unit/test_id.py
+++ tests/unit/test_id.py
@@ -11,6 +11,12 @@ class IdTests(TestCase):
         pyitt.id(domain)
         id_class_mock.assert_called_once_with(domain)
 
+    @pyitt_native_patch('Id')
+    def test_id_creation(self, id_class_mock):
+        domain = 'my domain'
+        pyitt.Id(domain)
+        id_class_mock.assert_called_once_with(domain)
+
 
 if __name__ == '__main__':
     unittest_main()  # pragma: no cover
