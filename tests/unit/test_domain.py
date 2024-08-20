db1e173f7c73ff6b15e2832f59596267e44ba164
Egor Suldin <rd3tap@yandex.ru>
2024-08-21 00:48:58 +0300

    Check the creation of Domain instance
    Add unit tests that check the creation of Domain instance.

--- tests/unit/test_domain.py
+++ tests/unit/test_domain.py
@@ -16,6 +16,17 @@ class DomainTests(TestCase):
         pyitt.domain(name)
         domain_class_mock.assert_called_once_with(name)
 
+    @pyitt_native_patch('Domain')
+    def test_domain_creation_without_arguments(self, domain_class_mock):
+        pyitt.Domain()
+        domain_class_mock.assert_called_once_with()
+
+    @pyitt_native_patch('Domain')
+    def test_domain_creation_with_name(self, domain_class_mock):
+        name = 'my domain'
+        pyitt.Domain(name)
+        domain_class_mock.assert_called_once_with(name)
+
 
 if __name__ == '__main__':
     unittest_main()  # pragma: no cover
