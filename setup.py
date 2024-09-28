9b00075a8779c14902a9ced417fbc36abb291cb6
Egor Suldin <rd3tap@yandex.ru>
2024-09-28 23:39:26 +0300

    Update version to 1.7.2

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ class NativeBuildExtension(build_ext):  # pylint: disable=R0903
 
 
 setup(name='pyitt',
-      version='1.7.1',
+      version='1.7.2',
       description='ITT API bindings for Python',
       packages=find_namespace_packages(include=['pyitt', 'pyitt.*']),
       ext_modules=[pyitt_native],
